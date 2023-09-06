af4b8c934e1240f8f258937304ad95d07de17338
ganga-matmari <136094077+ganga-matmari@users.noreply.github.com>
2023-09-06 11:59:14 +0530

    Add files via upload

---
+++ acronym.py
@@ -0,0 +1,13 @@
+##user_input = str(input("Enter a Phrase: "))
+##text = user_input.split()
+##a = " "
+##for i in text:
+##    a = a+str(i[0]).upper()
+##print(a)
+
+user_input = str(input("Enter a Phrase: "))
+text = user_input.split()
+a = ""
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)
